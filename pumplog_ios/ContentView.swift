d1ac807fd0dcbbd5327c5ef6fa8ec5fec4d907e5
k0rrr <neroxktzv2150a@gmail.com>
2026-08-07 20:49:09 +0900

    Initial Commit

---
+++ pumplog_ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pumplog_ios
+//
+//  Created by Kairi Tayama on 2026/08/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pumplog_ios/pumplog_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  pumplog_iosApp.swift
+//  pumplog_ios
+//
+//  Created by Kairi Tayama on 2026/08/07.
+//
+
+import SwiftUI
+
+@main
+struct pumplog_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
