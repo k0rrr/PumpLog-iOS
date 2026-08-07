cb0a4aacd49a8fb23a97cc4dc2ef40f9e809792d
k0rrr <neroxktzv2150a@gmail.com>
2026-08-08 08:26:07 +0900

    Add workout records

--- pumplog_ios/ContentView.swift
+++ pumplog_ios/ContentView.swift
@@ -7,12 +7,17 @@
 
 import SwiftUI
 
-
+struct WorkoutRecord : Identifiable {
+    let id = UUID()
+    let name : String
+    let weight : String
+    let reps : String
+}
 struct ContentView: View {
     @State private var exerciseName = ""
     @State private var exerciseWeight = ""
     @State private var exerciseReps = ""
-    
+    @State private var records : [WorkoutRecord] = []
     var body: some View{
         VStack{
             Text("PumpLog")
@@ -20,8 +25,23 @@ struct ContentView: View {
             TextField("重量", text: $exerciseWeight)
             TextField("回数", text: $exerciseReps)
             Button("記録する"){
-                print(exerciseName,exerciseWeight,exerciseReps)
+                let newRecord = WorkoutRecord(
+                    name: exerciseName,
+                    weight:exerciseWeight,
+                    reps: exerciseReps
+                )
+                
+                records.append(newRecord)
+                
+                exerciseName = ""
+                exerciseWeight = ""
+                exerciseReps = ""
             }
+            ForEach(records) { record in
+                Text("\(record.name)\(record.weight)kg×\(record.reps)回")
+        
+            }
+            
         }
     }
 }
